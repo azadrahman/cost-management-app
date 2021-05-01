0ab3e7d01d267a045e62c0364e9f514e9e3a5e9d
azadrahman <rehmansaaid@gmail.com>
2021-05-01 17:50:57 +0600

    1 -> created table view of users

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap';
-import Navbar from "./components/layouts/Header/Navbar"
+import React from "react";
+import Header from "./components/layouts/header";
+import PageHeader from "./components/layouts/PageHeader";
+import UserTable from "./components/layouts/UserTable";
+import SideMenu from "./components/layouts/SideMenu";
+import { makeStyles } from "@material-ui/core";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
 
-function App() {
+const useStyles = makeStyles({
+  appMain: {
+    paddingLeft: "0px",
+    width: "100%"
+  },
+  userTbl: {
+    margin: "1rem",
+    boxShadow: "4px 0px 6px 2px #ccc"
+  }
+});
+
+export default function App() {
+  const classes = useStyles();
   return (
-    <Container>
-      <Navbar />
-      <h2>Home page</h2>
-    </Container>
+    <>
+      {/* <SideMenu /> */}
+      <div className={classes.appMain}>
+        <Header />
+        <PageHeader
+          title="Users Table"
+          subTitle="Cost details of each user"
+          icon={<PeopleAltIcon fontSize="medium" />}
+        />
+        <div className={classes.userTbl}>
+          <UserTable />
+        </div>
+      </div>
+    </>
   );
 }
-
-export default App;
---
+++ src/services/userData.js
@@ -0,0 +1,35 @@
+const userDB = [
+    {
+      id: 1,
+      fullname: "Akib hasan",
+      email: "akib@example.com",
+      mobile: "+088 017711 7711"
+    },
+    {
+      id: 2,
+      fullname: "Sakib hasan",
+      email: "Sakib@example.com",
+      mobile: "+088 017711 7711"
+    },
+    {
+      id: 3,
+      fullname: "Rakib hasan",
+      email: "Rakib@example.com",
+      mobile: "+088 017711 7711"
+    },
+    {
+      id: 4,
+      fullname: "kibria hasan",
+      email: "kibria@example.com",
+      mobile: "+088 017711 7711"
+    },
+    {
+      id: 5,
+      fullname: "Simon hasan",
+      email: "simon@gmail.com",
+      mobile: "+088 018822 7711"
+    }
+  ];
+  
+  export default userDB;
+  
